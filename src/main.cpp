a23740296bc9a31dcb02daba4119e734268d22dd
vrees <viktor.rees@gmx.de>
2021-09-07 08:40:23 +0200

    stepper test added

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,45 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// Define pin connections & motor's steps per revolution
+const int dirPin = GPIO_NUM_32;
+const int stepPin = GPIO_NUM_33;
+const int stepsPerRevolution = 1200;
+
+void setup()
+{
+  // Declare pins as Outputs
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+
+  // digitalWrite(stepPin, HIGH);
+  // delay(20000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // Set motor direction clockwise
+  digitalWrite(dirPin, HIGH);
+
+  // Spin motor slowly
+  for (int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000); // Wait a second
+
+  // Set motor direction counterclockwise
+  digitalWrite(dirPin, LOW);
+
+  // Spin motor quickly
+  for (int x = 0; x < stepsPerRevolution; x++)
+  {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(1000); // Wait a second
 }
